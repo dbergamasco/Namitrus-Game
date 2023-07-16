9ac70000471e1611bc097ede44e2d739e04a8eb3
Diego Bergamasco <diegobergamasco92@gmail.com>
2023-07-16 23:14:07 +0200

    Update AttackDetails.cs

--- Assets/Scripts/Structs/AttackDetails.cs
+++ Assets/Scripts/Structs/AttackDetails.cs
@@ -2,15 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct AttackDetails
-{
-
-}
-
 [System.Serializable]
 public struct WeaponAttackDetails
 {
     public string attackName;
     public float movementSpeed;
     public float damageAmount;
+    public float comboTimer;
 }
