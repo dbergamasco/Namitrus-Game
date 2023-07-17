1bd27e8f684821cb4b16ccc0322504ac3a95cba6
Diego Bergamasco <diegobergamasco92@gmail.com>
2023-07-17 14:04:14 +0200

    Finished combat dummy

---
+++ Assets/Scripts/Interfaces/Particles/HitParticles.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitParticles : MonoBehaviour
+{
+    private void FinishAnim()
+    {
+        Destroy(gameObject);
+    }
+}
