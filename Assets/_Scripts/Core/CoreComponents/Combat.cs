6d02d6dc6da09abc1e51998b53ae0644d06c1201
Diego Bergamasco <diegobergamasco92@gmail.com>
2023-07-27 14:43:16 +0200

    Finished core components

---
+++ Assets/_Scripts/Core/CoreComponents/Combat.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat : CoreComponent, IDamageable, IKnockbackable
+{
+
+    [SerializeField] private GameObject damageParticles;
+
+    private Movement Movement => movement ?  movement : core.GetCoreComponent(ref movement);
+    private Movement movement;
+
+    private CollisionSenses CollisionSenses => collisionSenses ?  collisionSenses : core.GetCoreComponent(ref collisionSenses);
+    private CollisionSenses collisionSenses;
+
+    private Stats Stats { get => stats ??= core.GetCoreComponent<Stats>(); }
+    private Stats stats;
+    
+    private ParticleManager ParticleManager => particleManager ?  particleManager : core.GetCoreComponent(ref particleManager); 
+    private ParticleManager particleManager;
+
+    [SerializeField] private float maxKnockbackTime = 0.2f;
+
+    private bool isKnockbackActive;
+    private float knockbackStartTime;
+
+    public override void LogicUpdate()
+    {
+        CheckKnockback();
+    }
+
+    public void Damage(float amount)
+    {
+        Stats?.DecreaseHealth(amount);
+        ParticleManager?.StartParticlesWithRandomRotation(damageParticles);
+    }
+
+    public void Knockback(Vector2 angle, float strength, int direction)
+    {
+        Movement?.SetVelocity(strength, angle, direction);
+        Movement.CanSetVelocity = false;
+        isKnockbackActive = true;
+        knockbackStartTime = Time.time;
+    }
+
+    private void CheckKnockback()
+    {
+        if(isKnockbackActive && ((Movement.CurrentVelocity.y <= 0.01f && CollisionSenses.Ground) || Time.time >= knockbackStartTime + maxKnockbackTime))
+        {
+            isKnockbackActive = false;
+            Movement.CanSetVelocity = true;
+        }
+    }
+}
---
+++ Assets/_Scripts/Core/CoreComponents/Stats.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class Stats : CoreComponent
+{
+    public event Action OnHealthZero;
+
+    [SerializeField] private float maxHealth;
+    private float currentHealth;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        currentHealth = maxHealth;
+    }
+
+    public void DecreaseHealth(float amount)
+    {
+        currentHealth -= amount;
+
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            
+            OnHealthZero?.Invoke();
+        }
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    }
+}
